1512e8c9c720d7fd785d821f216c8234c8c05da3
Karthik Ram <karthikramx@Karthiks-MacBook-Pro.local>
2023-02-16 21:58:46 -0700

    initail commit

---
+++ Assignment 1.R
@@ -0,0 +1,26 @@
+
+
+# Data
+# 1. Daily Stock Data
+# 2. Fundamental Data
+# 3. Sentiment Data
+# 4. Economic Data
+# 5. S&P500 index data
+
+# Data Manipulation and Organization
+# 1. Get log returns of all stocks in the portfolio
+# 2. Organize fundamental data , join data frames to match index (date)
+# 3. Perform sentiment analysis and merge sentiment scores in the dataframe
+# 4. Merge economic data
+# 5. Merge SP500 daily change percentage column (y)
+
+# Add additional indicators (optional for now)
+# 1. Technical indicators,
+# 2. Momentum indicators
+# 3. ....
+
+
+# Factor Model
+# 1. Run linear regression on factors
+# 2. Run lm.summary() and select the significant factors
+# 3. Drop factor which are not significant from the data frame
